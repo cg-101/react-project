2ff2afa2f224d564c4bf851bdeee42b7987f2d5b
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-09 10:30:32 +0530

    navigte progammatically.

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,7 +1,27 @@
+import { Link, useHistory } from "react-router-dom";
+
 export default function Page1() {
+  let history = useHistory();
+
+  const goToPage3 = () => {
+    // logic
+    // logic
+    // ... more logic
+    // navigate to page2 programmatically
+    history.push("/p3");
+  };
+
   return (
     <div className="bg-primary" style={{ height: "100vh" }}>
       PAGE1
+      {/**Option` */}
+      <div></div>
+      <Link to="/p2">
+        <button>Go To Page2 Option 1</button>
+      </Link>
+      {/**Option2 */}
+      <div></div>
+      <button onClick={() => goToPage3()}>Go to Page3 Option 2</button>
     </div>
   );
 }
