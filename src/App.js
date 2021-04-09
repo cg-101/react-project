3a863f284cd94660802d16087796152b88135ce6
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-09 10:02:47 +0530

    routing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // SPA :: SINGLE PAGE APPLICATION
 function App() {
   // UI PART
   return (
+    <Router>
+      <Route path="/p1" component={Page1} />
+      <Route path="/p2" component={Page2} />
+      <Route path="/p3" component={Page3} />
+      <Route path="/" component={Page1} />
+    </Router>
+  );
+}
+
+function Page1() {
+  return (
     <div className="bg-primary" style={{ height: "100vh" }}>
-      Hello World
+      PAGE1
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div className="bg-info" style={{ height: "100vh" }}>
+      PAGE2
+    </div>
+  );
+}
+
+function Page3() {
+  return (
+    <div className="bg-warning" style={{ height: "100vh" }}>
+      PAGE3
     </div>
   );
 }
