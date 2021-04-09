8b09f47af99fe6f94c8ec850553e2245fb3b8062
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-09 10:09:20 +0530

    swithc udpate.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
@@ -10,10 +10,12 @@ function App() {
   // UI PART
   return (
     <Router>
-      <Route path="/p1" component={Page1} />
-      <Route path="/p2" component={Page2} />
-      <Route path="/p3" component={Page3} />
-      <Route path="/" component={Page1} />
+      <Switch>
+        <Route path="/p1" component={Page1} />
+        <Route path="/p2" component={Page2} />
+        <Route path="/p3" component={Page3} />
+        <Route path="/" component={Page1} />
+      </Switch>
     </Router>
   );
 }
